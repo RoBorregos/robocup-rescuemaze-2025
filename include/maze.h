b608555e95744323c1cdfd3d635172eb3877d9da
biweep863 <a01286400@tec.mx>
2025-02-28 13:42:20 -0600

    Update maze.h

--- include/maze.h
+++ include/maze.h
@@ -7,14 +7,6 @@
 
 class maze{
     public:
-        /*
-        void ahead();
-        void right();
-        void left();
-        void rotate(const int targetOrientation);
-        bool vlx(int num);
-        bool isWall(const TileDirection& direction);
-        */
         maze();
         void followPath(Stack& path);
         void dijkstra(coord& start, coord& end, arrCustom<coord>& tilesMap, arrCustom<Tile> tiles);
