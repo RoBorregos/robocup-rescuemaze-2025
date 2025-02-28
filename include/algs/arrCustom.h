f48adc8a62da72abef98c9499c5ad96a04313a6b
biweep863 <a01286400@tec.mx>
2025-02-28 11:37:17 -0600

    Update arrCustom.h

--- include/algs/arrCustom.h
+++ include/algs/arrCustom.h
@@ -1,52 +1,66 @@
 // CustomArray.h
-#include <cstddef>
 template <typename T>
 class arrCustom {
 private:
-    int i = 0;
-    T* positions;
+    uint8_t i = 0;
     size_t size;
     T absValue;
+    T* positions;
 
 public:
     // Constructor
     arrCustom(size_t size, T value) : size(size), absValue(value) {
         if (size <= 0) {
             this->size = 0;
-            positions = nullptr;
+            // positions = nullptr;
+            positions = new T[0];
             return;
         }
         positions = new T[size]; 
-        for(int i = 0; i < size; i++){
+        for(uint8_t i = 0; i < size; i++){
             positions[i] = value;
         }
     }
     void set(size_t index, T value) {
         if (index < size && index >= 0) {
             positions[index] = value;
+        }else{
+            Serial.println("Index out of bounds");
         }
     }
-    int getIndex(T value){
-        for(int i = 0; i < size; i++){
+    uint8_t getIndex(T value){
+        for(uint8_t i = 0; i < size; i++){
             if(positions[i] == value){
                 return i;
             }
         }
-        return -1;
+        return 255;
     }
     T& getValue(size_t index) const {
         if (index < size && index >= 0) {
             return positions[index];
         }
-        return positions[0];
+        /*
+        else{
+            Serial.println("Index out of bounds");
+            return absValue;
+        }*/
     }
     size_t getSize() const {
         return size;
     }
     //just for som arrays using push_back
-    void push_back(T position){
-        this->positions[i] = position;
+    void push_back(T position) {
+        if (i >= size) {
+            // correct this
+            T* temp = new T[size + 1];
+            for (uint8_t j = 0; j < size; j++) {
+                temp[j] = positions[j];
+            }
+            positions = temp;
+            size++;
+        }
+        positions[i] = position;
         i++;
     }
 };
-
